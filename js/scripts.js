2a4af656b1961a8ae1916878040fdd79ff5e48fd
tmillot08 <thomas.millot08@gmail.com>
2018-06-26 09:26:57 +0200

    qsvd

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+
+
+$(document).ready(function(){
+  $('#fname, #name, #company, #adress, #city').on('input', function() {
+	   var input=$(this);
+     var re = /[A-Z]/i
+	   var is_name=re.test(input.val());
+	   if(is_name ){
+       input.removeClass("error").addClass("valid");
+     }else{
+
+       input.removeClass("valid").addClass("error");
+     }
+});
+$('#number').on('input', function() {
+   var input=$(this);
+   var re = /[0-9]/i;
+   var is_number=re.test(input.val());
+   if(is_number){
+    input.removeClass("error").addClass("valid");
+    }else{
+    input.removeClass("valid").addClass("error");
+    }
+});
+
+$('#zip').on('input', function() {
+   var input=$(this);
+   var re = /[0-9]{5}/i;
+   var is_number=re.test(input.val());
+   if(is_number){
+    input.removeClass("error").addClass("valid");
+    }else{
+    input.removeClass("valid").addClass("error");
+    }
+});
+$('#country').on('input', function() {
+  	   var input=$(this);
+
+  	   if(input.attr('option', 'UK') || input.attr('option', 'US') ){
+          input.removeClass("error").addClass("valid");
+       }else{
+          input.removeClass("valid").addClass("error");
+
+       }
+});
+$('#d1, #d2').onclick('input', function() {
+  	   if($('#d1').is(':checked') || $('#d2').is(':checked') ){
+          input.removeClass("valid").addClass("error");
+       }else{
+
+          input.removeClass("error").addClass("valid");
+       }
+});
+
+
+
+
+});
---
+++ post.php
@@ -0,0 +1,5 @@
+<?php
+	$array = $_POST;
+	foreach($array as $value)
+	echo $value . '<br>';
+?>
